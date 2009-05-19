a89c94ee22dea86250e884ab1e59be0443085115
Greg Newman <greg@20seven.org>
2009-05-19 08:31:27 -0400

    adding menu and menu items to admin

--- admin.py
+++ admin.py
@@ -9,6 +9,16 @@ class PageAdmin(VersionAdmin):
     list_filter = ('published',)
     prepopulated_fields = {'slug': ('title',)}
     save_on_top = True
+
+class MenuAdmin(VersionAdmin):
+    ordering = ('title',)
+    list_display = ('title', 'published', 'created_on')
+    list_filter = ('published',)
+    save_on_top = True
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_on',)
     
 admin.site.register(get_model('seamoss', 'page'), PageAdmin)
-
+admin.site.register(get_model('seamoss', 'menu'), MenuAdmin)
+admin.site.register(get_model('seamoss', 'menuitem'), MenuItemAdmin)
