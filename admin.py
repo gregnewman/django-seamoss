8873f405e27b7b817fd413dfd1cbdde19d654403
Greg Newman <greg@20seven.org>
2009-05-19 07:42:14 -0400

    adding admin for pages

--- admin.py
+++ admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 from models import *
+from django.db.models import get_model
 from reversion.admin import VersionAdmin
+
+class PageAdmin(VersionAdmin):
+    ordering = ('title',)
+    list_display = ('title', 'published', 'created_on')
+    list_filter = ('published',)
+    prepopulated_fields = {'slug': ('title',)}
+    save_on_top = True
+    
+admin.site.register(get_model('seamoss', 'page'), PageAdmin)
+
