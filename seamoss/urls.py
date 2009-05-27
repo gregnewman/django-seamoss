3e79845cff3a364839c73d281bca36716df50c66
Greg Newman <greg@20seven.org>
2009-05-27 11:34:08 -0400

    page urls

--- seamoss/urls.py
+++ seamoss/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
+from django.core.urlresolvers import reverse
 
 
 urlpatterns = patterns('',
-    # (r'^seamoss/', include('seamoss.foo.urls')),
+    url(r'^(?P<slug>[-_\w]+)/$', 'seamoss.views.render_page', name="render-page"),
+    url(r'^$', 'seamoss.views.home_page', name="home-page"),
 )
