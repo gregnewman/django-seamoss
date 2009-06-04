8a3a8f782940f323d01564fdb64e54a2d66e69e9
Greg Newman <greg@20seven.org>
2009-06-04 04:06:45 -0400

    let setup.py get the latest tagging

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author='Greg Newman',
     author_email='greg@20seven.org',
     url='https://github.com/gregnewman/django-seamoss/tree/master',
-    requires=(
-        'tagging (>0.2.1)',
+    setup_requires=(
+        'tagging', # (>0.2.1)',
         'django_mptt (>0.2.1)',
         'reversion (>1.1.1)',
     ),
