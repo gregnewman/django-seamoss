dca8d40c8c5b94394b4b1ce035188774e4237960
Greg Newman <greg@20seven.org>
2009-06-23 07:08:38 -0400

    adding MenuItem form to hide mptt fields

--- seamoss/forms.py
+++ seamoss/forms.py
@@ -2,10 +2,17 @@ from datetime import datetime
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-from seamoss.models import Page
+from seamoss.models import Page, MenuItem
         
 class PageForm(forms.ModelForm):
     
     class Meta:
         model = Page
         exclude = ('markup',)
+        
+        
+class MenuItemForm(forms.ModelForm):
+
+    class Meta:
+        model = MenuItem
+        exclude = ('lft', 'rght', 'tree_id', 'level', )
